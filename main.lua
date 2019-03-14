c308f7f6842f37862ece82bdac6ac9f69d6f7e90
sophia <sophiabaldonado@gmail.com>
2019-03-14 11:48:33 -0700

    WIP fade out items when not currently displaying;

--- main.lua
+++ main.lua
@@ -4,11 +4,25 @@ local flux = require 'flux'
 
 function lovr.load()
 	itemSize = .6
-	files = fs.getDirectoryItems('saved/')
 	model = lovr.graphics.newModel('file.glb')
+	files = fs.getDirectoryItems('saved/')
+
 	scroll = 0
 	scrollSpacing = { y = 0 }
-	numFilesToDisplay = 1
+	numFilesToDisplay = 3
+
+	opacities = {}
+	for i, file in ipairs(files) do
+		if i <= numFilesToDisplay then
+			opacities[file] = { a = 1 }
+		else
+			opacities[file] = { a = 0}
+		end
+	end
+	upperEdge = nil
+	lowerEdge = nil
+	nextUpperEdge = nil
+	nextLowerEdge = nil
 
 	transform = lovr.math.mat4()
 	local hx, hy, hz = lovr.headset.getPosition()
@@ -20,6 +34,41 @@ end
 
 function lovr.update(dt)
 	flux.update(dt)
+
+	for i, file, x, y in items() do
+		if (i - scroll) == 1 then
+			setUpperEdges(file, files[1 - 1])
+		end
+		if i == scroll + numFilesToDisplay + 1 then
+			setLowerEdges(file, files[1 + 1])
+		end
+	end
+end
+
+function setUpperEdges(file, prevFile)
+	if file ~= upperEdge then
+		upperEdge = file
+	end
+	if prevFile and prevFile ~= nextUpperEdge then
+		nextUpperEdge = prevFile
+	end
+end
+
+function getUpperEdge()
+	return upperEdge
+end
+
+function getNextLowerEdge()
+	return nextLowerEdge
+end
+
+function setLowerEdges(file, nextFile)
+	if file ~= lowerEdge then
+		lowerEdge = file
+	end
+	if nextFile and nextFile ~= nextLowerEdge then
+		nextLowerEdge = nextFile
+	end
 end
 
 function lovr.draw()
@@ -33,18 +82,18 @@ function lovr.draw()
 			a = 1
 			zOff = .05
 		end
-			y = y + scrollSpacing.y
-			local model = model
-			local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
-			local width, height, depth = maxx - minx, maxy - miny, maxz - minz
-			local scale = itemSize / math.max(width, height, depth)
-			local cx, cy, cz = (minx + maxx) / 2 * scale, (miny + maxy) / 2 * scale, (minz + maxz) / 2 * scale
-			lovr.graphics.setColor(.2, .1, .3, a - .4)
-			model:draw(x - cx, y - cy, 0 - cz + zOff, scale, (3 * math.pi) / 2, 0, 1, 0)
 
-			lovr.graphics.setColor(1, 1, 1, a - .2)
-			lovr.graphics.print(string.gsub(file, '.json', ''), x - cx + .1, y - cy, 0 - cz + .02 + zOff, scale * .6)
-		-- end
+		y = y + scrollSpacing.y
+		local model = model
+		local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
+		local width, height, depth = maxx - minx, maxy - miny, maxz - minz
+		local scale = itemSize / math.max(width, height, depth)
+		local cx, cy, cz = (minx + maxx) / 2 * scale, (miny + maxy) / 2 * scale, (minz + maxz) / 2 * scale
+		lovr.graphics.setColor(.2, .1, .3, opacities[file].a)
+		model:draw(x - cx, y - cy, 0 - cz + zOff, scale, (3 * math.pi) / 2, 0, 1, 0)
+
+		lovr.graphics.setColor(1, 1, 1, a - .2)
+		lovr.graphics.print(string.gsub(file, '.json', ''), x - cx + .1, y - cy, 0 - cz + .02 + zOff, scale * .6)
 	end
 
 	lovr.graphics.pop()
@@ -61,7 +110,9 @@ end
 
 function moveUp()
 	if (scroll + 1) > (#files - numFilesToDisplay) then return end
-	
+
+	transitionUp()
+
 	scroll = scroll + 1
 	flux.to(scrollSpacing, .5, { y = scrollSpacing.y + (itemSize * .3) }):ease('cubicout')
 end
@@ -69,9 +120,29 @@ end
 function moveDown()
 	if (scroll - 1) < 0 then return end
 
+	transitionDown()
+
 	scroll = scroll - 1
 	flux.to(scrollSpacing, .5, { y = scrollSpacing.y - (itemSize * .3) }):ease('cubicout')
 	-- scrollSpacing = scrollSpacing - (itemSize * .3)
+end
+
+function transitionUp()
+	if opacities[upperEdge] then
+		flux.to(opacities[upperEdge], .5, { a = 0 }):ease('cubicout')
+	end
+	if opacities[nextLowerEdge] then
+		flux.to(opacities[nextLowerEdge], .5, { a = 1 }):ease('cubicout')
+	end
+end
+
+function transitionDown()
+	if opacities[nextUpperEdge] then
+		flux.to(opacities[nextUpperEdge], .5, { a = 1 }):ease('cubicout')
+	end
+	if opacities[lowerEdge] then
+		flux.to(opacities[lowerEdge], .5, { a = 0 }):ease('cubicout')
+	end
 end
 
 function items()
